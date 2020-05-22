8a2008ec5b3293fe1550bd6d68b008de1a14e720
Siddharth Rao <siddrao.926@gmail.com>
2020-05-22 04:38:17 -0700

    Added 404 to Category Endpoint

--- src/server.js
+++ src/server.js
@@ -1,16 +1,20 @@
 import express from "express"
 import data from './data.json'
+import {isEmpty} from 'lodash';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/all', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(data);
 });
 
 app.get('/category/:categoryName', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(data[req.params.categoryName]);
+
+    let result = data[req.params.categoryName];
+
+    !isEmpty(result) ? res.status(200).json(result) : res.status(404).send('Data Was Not Found'); 
 });
 
 app.listen(8000, () => console.log('Listening on PORT 8000'));
