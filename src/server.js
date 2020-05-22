8c742f6eccb7a27595810e09d829f77b747a43c3
Siddharth Rao <siddrao.926@gmail.com>
2020-05-21 21:45:11 -0700

    Added Category Return

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
-import express from "express";
+import express from "express"
+
+import animalData from './assets/animal.json';
+import antiMalwareData from './assets/anti-malware.json';
 
 const app = express();
 
+/*
+|                  |
+V TODO: Return All V
+
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.status(200).json();
+});
+*/
+
+app.get('/category/:categoryName', (req, res) => {
+    res.status(200).sendFile(__dirname + '/assets/' + req.params.categoryName + '.json');
 });
 
 app.listen(8000, () => console.log('Listening on PORT 8000'));
