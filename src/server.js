b919f9ddb0b206cf6cb61b77aaca924ff26549bc
Siddharth Rao <siddrao.926@gmail.com>
2020-05-22 20:58:18 -0700

    Forrmatted With Prettier

--- src/server.js
+++ src/server.js
@@ -1,20 +1,22 @@
-import express from "express"
-import data from './data.json'
-import {isEmpty} from 'lodash';
+import express from "express";
+import data from "./data.json";
+import { isEmpty } from "lodash";
 
 const app = express();
 
-app.get('/all', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(data);
+app.get("/all", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).json(data);
 });
 
-app.get('/category/:categoryName', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
+app.get("/category/:categoryName", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
 
-    let result = data[req.params.categoryName];
+  let result = data[req.params.categoryName];
 
-    !isEmpty(result) ? res.status(200).json(result) : res.status(404).send('Data Was Not Found'); 
+  !isEmpty(result)
+    ? res.status(200).json(result)
+    : res.status(404).send("Data Was Not Found");
 });
 
-app.listen(8000, () => console.log('Listening on PORT 8000'));
+app.listen(8000, () => console.log("Listening on PORT 8000"));
